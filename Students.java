74a96261d21b6e75170851eb1e4d60a0d648741b
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-12-18 18:05:18 +0500

    file added

---
+++ Students.java
@@ -0,0 +1,25 @@
+class Students{
+    int StudentID;
+    String StudentName;
+    String StudentCity;
+
+    public void study(){
+        System.out.println(StudentName+" is studying");
+    }
+    public void ShowInfo(){
+        System.out.println("My ID is "+StudentID);
+        System.out.println("My name is "+StudentName);
+        System.out.println("My City is "+StudentCity);
+
+    }
+
+    public static void main(String[] args){
+        Students s1;
+        s1=new Students;
+        s1.StudentID=16;
+        s1.StudentName="Sohail";
+        s1.StudentCity="Hyderabad";
+        s1.study();
+        s1.ShowInfo();
+    }
+}
